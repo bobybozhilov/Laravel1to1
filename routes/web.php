0f4e62c2e77884ddd2b5a1603dd725856d0ccbb4
bobybozhilov <bobybozhilov@abv.bg>
2017-05-24 23:28:56 +0300

    Chapter 12 completed
    added insert,read,update,delete

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,38 @@
 |
 */
 
-Route::get('/', function () {
+use App\Address;
+use App\User;
+
+Route ::get('/', function () {
     return view('welcome');
+});
+
+Route ::get('/insert', function () {
+
+    $user = User ::findOrFail(1);
+    $address = new Address(['name' => '1234 houson ave']);
+    $user -> address() -> save($address);
+});
+
+Route ::get('/update',function (){
+//    $address = Address::where('user_id','=',1);
+//    $address = Address::whereUserId(1);
+    $address = Address::where('user_id',1)->first();
+    $address->name="Updated new Address";
+    $address->save();
+});
+
+Route ::get('/read', function (){
+
+    $user = User::findOrFail(1);
+    echo $user->address->name;
+
+});
+
+Route ::get('/delete', function (){
+
+    $user = User::findOrFail(1);
+     $user->address()->delete();
+    return 'deleted';
 });
